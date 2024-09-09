e395f666d0f66093ff97592b1e850c2b015c4db6
Joshua Goins <josh@redstrate.com>
2024-09-09 17:20:35 -0400

    Add some initial KAboutData

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <KAboutData>
 #include <KLocalizedString>
 #include <QApplication>
 
@@ -11,6 +12,20 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     KLocalizedString::setApplicationDomain(QByteArrayLiteral("kirigami-workbench"));
+
+    KAboutData about(QStringLiteral("kirigami-workbench"),
+                     i18n("Kirigami Workbench"),
+                     QStringLiteral("0.1"), // TODO: replace with ECM version
+                     i18n("Quickly prototype in QML"),
+                     KAboutLicense::GPL_V3,
+                     QStringLiteral("© 2024 Joshua Goins"));
+    about.addAuthor(QStringLiteral("Joshua Goins"),
+                    QStringLiteral("Maintainer"),
+                    QStringLiteral("josh@redstrate.com"),
+                    QStringLiteral("https://redstrate.com/"),
+                    QUrl(QStringLiteral("https://redstrate.com/rss-image.png")));
+
+    KAboutData::setApplicationData(about);
 
     MainWindow w;
     w.show();
